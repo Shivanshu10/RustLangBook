7d86bbf95ebb652d9401d78df5a41e769fdb9ec6
Shivanshu Mishra <smishra_be20@thapar.edu>
2023-08-15 02:06:58 -0700

    init

---
+++ 18-crate/art/target/package/my_art-0.1.0/target/debug/deps/my_art-e1af87ff6f961813.d
@@ -0,0 +1,7 @@
+C:\Users\sm101\Documents\projects\RustBook\18-crate\art\target\package\my_art-0.1.0\target\debug\deps\my_art-e1af87ff6f961813.rmeta: src\lib.rs
+
+C:\Users\sm101\Documents\projects\RustBook\18-crate\art\target\package\my_art-0.1.0\target\debug\deps\libmy_art-e1af87ff6f961813.rlib: src\lib.rs
+
+C:\Users\sm101\Documents\projects\RustBook\18-crate\art\target\package\my_art-0.1.0\target\debug\deps\my_art-e1af87ff6f961813.d: src\lib.rs
+
+src\lib.rs:
