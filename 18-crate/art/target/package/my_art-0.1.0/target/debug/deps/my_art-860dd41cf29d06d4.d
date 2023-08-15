3c2fce919054ec079863e2224a3a8f4649c98e4d
Shivanshu Mishra <smishra_be20@thapar.edu>
2023-08-15 02:05:25 -0700

    init

--- 18-crate/art/target/package/my_art-0.1.0/target/debug/deps/my_art-860dd41cf29d06d4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sm101\Documents\projects\RustBook\18-crate\art\target\package\my_art-0.1.0\target\debug\deps\my_art-860dd41cf29d06d4.exe: src\main.rs
-
-C:\Users\sm101\Documents\projects\RustBook\18-crate\art\target\package\my_art-0.1.0\target\debug\deps\my_art-860dd41cf29d06d4.d: src\main.rs
-
-src\main.rs:
